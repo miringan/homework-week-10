f749314811fb5fe5916a7678c2161d6612524476
Michael Iringan <miringan@uw.edu>
2022-01-13 19:51:15 -0800

    Added code to index.js and generator.js.

---
+++ index.js
@@ -0,0 +1,31 @@
+// Actions needed
+
+// Ask them for manager info
+
+    // Prompt the user for the data
+
+        // Then create and store an object for the Manager
+
+        // Then ask what they would like to do next
+
+// Ask them for engineer info
+
+// Ask them for intern info
+
+// Ask what they would like to do next
+
+    // Add employee, add intern, be done
+
+        // If add engineer => ask them for engineer info
+        
+        // If add intern => ask them for intern info
+
+        // If be done => build an html page
+
+// Use all of the collected employee data to build an html page
+
+const Generator = require('./lib/generator');
+
+const generator = new Generator();
+
+//generator.run()
